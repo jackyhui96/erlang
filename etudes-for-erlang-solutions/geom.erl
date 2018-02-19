dbe70a2bc5a302f22e8d41542c491bc6d3c50aac
Jacky Hui <halojax@hotmail.com>
2018-02-19 14:41:10 +0000

    Update geom.erl

--- etudes-for-erlang-solutions/geom.erl
+++ etudes-for-erlang-solutions/geom.erl
@@ -2,8 +2,8 @@
 %%% @author jackyhui
 %%% @copyright (C) 2016, <COMPANY>
 %%% @doc
-%%% Chapter: 2-01
-%%% Find the area of a rectangle using it's length and width
+%%% Chapter: 2
+%%% Functions for calculating areas of geometric shapes.
 %%% @end
 %%% Created : 19. Feb 2018 14:30
 %%%-------------------------------------------------------------------
@@ -12,5 +12,9 @@
 
 -export([area/2]).
 
+%% @doc Calculates the area of a rectangle,
+%% given the length and width. 
+%% Returns the product of its args
+-spec area(number(), number()) -> number().
 area(Length, Width) ->
     Length * Width.
